c37a1aab0f55d266e6c6f0b2210e49c091d9b4bd
Pedro Mac <vaz.valois@df.ufscar.br>
2020-06-26 09:43:06 -0300

    spectra data augmentation with spline added

--- mango/helpers/preprocessing.py
+++ mango/helpers/preprocessing.py
@@ -1,7 +1,38 @@
+from collections import defaultdict
+
+import pandas as pd
+import numpy as np
+
 from sklearn.preprocessing import OneHotEncoder
+from scipy.interpolate import interp1d
 
 
 def one_hot_encode(arr):
     enc = OneHotEncoder(handle_unknown='ignore')
     enc.fit(arr)
     return enc.transform(arr).toarray()
+
+def spectra_data_augmentation(arr, n: int, *args, **kwargs):
+    l = arr.shape[1]  # graph length
+
+    x = np.linspace(0, 1, num=l, endpoint=True)
+    new_x = np.linspace(0, 1, num=l * n, endpoint=True)
+
+    def interpolate(y):
+        f = interp1d(x, y, *args, **kwargs)
+        return f(new_x)
+    
+    result = []
+    for graph in arr:
+        new_graph = interpolate(graph)
+
+        splits = defaultdict(list)
+        for i, v in enumerate(new_graph):
+            splits[i % n].append(v)
+
+        result.append(
+            pd.DataFrame(splits)
+        )
+
+    return result
+
--- src.py
+++ src.py
@@ -1,55 +1,41 @@
 import os
 import glob
 import numbers
+from collections import defaultdict
 
 import pandas as pd
 import numpy as np
+from scipy.interpolate import interp1d
 
 from enum import Enum
 
-class classes_enum(Enum):
-    none = ''
-    antrac = 'A'
-    collapse = 'C'
-    antrac_and_collapse = 'AC'
+df = pd.read_parquet('data/mango_spectra.parquet')
 
-def get_class_from_path(filepath: str) -> classes_enum:
-    basename = os.path.basename(filepath)
-    name = os.path.splitext(basename)[0]
+
+def spectra_data_augmentation(arr, n: int, *args, **kwargs):
+    l = arr.shape[1]  # graph length
+
+    x = np.linspace(0, 1, num=l, endpoint=True)
+    new_x = np.linspace(0, 1, num=l * n, endpoint=True)
+
+    def interpolate(y):
+        f = interp1d(x, y, *args, **kwargs)
+        return f(new_x)
     
-    fruto, n, *classes = name.split('_')
+    result = []
+    for graph in arr:
+        new_graph = interpolate(graph)
 
-    assert fruto == 'Fruto', "first symbol is not 'Fruto'."
-    assert isinstance(int(n), numbers.Integral), "second symbol is not a Integer"
+        splits = defaultdict(list)
+        for i, v in enumerate(new_graph):
+            splits[i % n].append(v)
 
-    if len(classes) == 1:
-        return classes_enum.antrac if classes[0] == classes_enum.antrac else classes_enum.collapse
-    else:
-        switch = {
-            0: classes_enum.none,
-            2: classes_enum.antrac_and_collapse
-        }
-        return switch[len(classes)]
+        result.append(
+            pd.DataFrame(splits)
+        )
 
-files = glob.glob1('data', '*.xls')
+    return pd.concat(result, axis=1).T.reset_index(drop=True)
 
-classes = list(map(get_class_from_path, files))
+arr = df.drop(['antrac', 'collapse'], axis=1).values
 
-has_antrac = [c == classes_enum.antrac or c == classes_enum.antrac_and_collapse for c in classes]
-has_collapse = [c == classes_enum.collapse or c == classes_enum.antrac_and_collapse for c in classes]
-
-
-data = []
-for f, a, c in zip(files, has_antrac, has_collapse):
-    path = os.path.join('data', f)
-    print(path)
-    
-    df = pd.read_excel(path).drop(['λ(nm)', 'Áreas', 'Parâmetros: TI(ms), Md, Bx'], axis=1).T
-    df['antrac'] = a
-    df['collapse'] = c
-
-    data.append(df)
-    
-final_df = pd.concat(data).reset_index(drop=True)
-final_df.columns = final_df.columns.astype(str)
-final_df.to_parquet('mango_spectra.parquet')
+new_arr = spectra_data_augmentation(arr, n=10)
